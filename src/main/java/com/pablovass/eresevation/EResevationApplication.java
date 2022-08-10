2052299a65882d358de2f4e9b31e577b745d7407
pablovass <faktory.fm@gmail.com>
2022-08-10 19:31:01 -0300

    app

---
+++ src/main/java/com/pablovass/eresevation/EResevationApplication.java
@@ -0,0 +1,13 @@
+package com.pablovass.eresevation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EResevationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EResevationApplication.class, args);
+	}
+
+}
