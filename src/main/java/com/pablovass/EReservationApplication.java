8d41ab31a3c491aebcb422925fb66d396eeb4f8c
pablovass <faktory.fm@gmail.com>
2022-08-29 20:03:44 -0300

    swagger config y funcionando

--- src/main/java/com/pablovass/EReservationApplication.java
+++ src/main/java/com/pablovass/EReservationApplication.java
@@ -2,12 +2,14 @@ package com.pablovass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @SpringBootApplication
 public class EReservationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EReservationApplication.class, args);
+		SpringApplication.run(EReservationApplication.class,args);
 	}
 
 }
--- src/main/java/com/pablovass/vista/resources/ClienteResource.java
+++ src/main/java/com/pablovass/vista/resources/ClienteResource.java
@@ -1,10 +1,16 @@
 /**
- * 
+ *
  */
 package com.pablovass.vista.resources;
 
 import java.util.List;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -19,10 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 import com.pablovass.modelo.Cliente;
 import com.pablovass.negocio.services.ClienteService;
 import com.pablovass.vista.resources.vo.ClienteVO;
-//import io.swagger.annotations.Api;
-//import io.swagger.annotations.ApiOperation;
-//import io.swagger.annotations.ApiResponse;
-//import io.swagger.annotations.ApiResponses;
+
+
 /**
  * Clase que representa el servicio web del cliente 
  * @author pablovass
@@ -30,69 +34,81 @@ import com.pablovass.vista.resources.vo.ClienteVO;
  */
 @RestController
 @RequestMapping("/api/cliente")
-//@Api("cliente")
 public class ClienteResource {
-	private final ClienteService clienteService;
+    private final ClienteService clienteService;
 
-	public ClienteResource(ClienteService clienteService) {
-		this.clienteService = clienteService;
-	}
-/**
- * 
- * @param clienteVo
- * @return
- */
-	@PostMapping
-//	@ApiOperation(value="Crear Cliente", notes="Servicio para crear un nuevo cliente")
-//	@ApiResponses(value= {@ApiResponse(code=201, message="Cliente creado correctamente"),
-//			@ApiResponse(code=400, message="Solisitud Ivalida")})
-	public ResponseEntity<Cliente>createCliente(@RequestBody ClienteVO clienteVo){
-		Cliente cliente=new Cliente();
-		cliente.setNombreCli(clienteVo.getNombreCli());
-		cliente.setApellidoCli(clienteVo.getApellidoCli());
-		cliente.setDireccionCli(clienteVo.getDireccionCli());
-		cliente.setTelefonoCli(clienteVo.getTelefonoCli());
-		cliente.setEmailCli(clienteVo.getEmailCli());
-		return new ResponseEntity<>(this.clienteService.create(cliente),HttpStatus.CREATED);
-	}
-	@PutMapping("/{identificacion}")
-//	@ApiOperation(value="Actualizar Cliente", notes="Servicio para actulizar un nuevo cliente")
-//	@ApiResponses(value= {@ApiResponse(code=201, message="Cliente actualizado correctamente"),
-//			@ApiResponse(code=404, message="Cliente no encotrado")})
-	public ResponseEntity<Cliente>updateCliente(@PathVariable("identificacion")String identificacion,ClienteVO clienteVo){
-		Cliente cliente=this.clienteService.findByIdentificacion(identificacion);
-		if (cliente ==null) {
-			return new ResponseEntity<Cliente>(HttpStatus.NOT_FOUND);
-		}else {
-			cliente.setNombreCli(clienteVo.getNombreCli());
-			cliente.setApellidoCli(clienteVo.getApellidoCli());
-			cliente.setDireccionCli(clienteVo.getDireccionCli());
-			cliente.setTelefonoCli(clienteVo.getTelefonoCli());
-			cliente.setEmailCli(clienteVo.getEmailCli());
-			
-		}
-		return new ResponseEntity<>(this.clienteService.update(cliente),HttpStatus.OK);
-	}
-	/**
-	 * metodo que borra el cliente 
-	 * @param identificacion
-	 */
-	@DeleteMapping("/{identificacion}")
-//	@ApiOperation(value="Cliente Eliminado", notes="Servicio para eliminar un cliente")
-//	@ApiResponses(value= {@ApiResponse(code=201, message="Cliente eliminado correctamente"),
-//			@ApiResponse(code=404, message="Cliente no encontrado")})
-	public void removeCliente(@PathVariable("identificacion")String identificacion) {
-		//realizamos la consulta del cliente 
-		Cliente cliente=this.clienteService.findByIdentificacion(identificacion);
-		if(cliente != null) {
-			this.clienteService.delete(cliente);
-		}
-	}
-	@GetMapping
-//	@ApiOperation(value="Listar Cliente", notes="Servicio para listar todos los clientes")
-//	@ApiResponses(value= {@ApiResponse(code=201, message="Clientes Encontrados"),
-//			@ApiResponse(code=404, message="Clientes no encontrados")})
-	public ResponseEntity<List<Cliente>>findAll(){
-		return ResponseEntity.ok(this.clienteService.findAll());
-	}
+    public ClienteResource(ClienteService clienteService) {
+        this.clienteService = clienteService;
+    }
+
+    /**
+     *
+     * @param clienteVo
+     * @return
+     */
+    @PostMapping
+   // @ApiOperation(value = "Crear Cliente", notes = "Servicio para crear un nuevo cliente")
+    //@ApiResponses(value = {@ApiResponse(code = 201, message = "Cliente creado correctamente"),
+      //      @ApiResponse(code = 400, message = "Solisitud Ivalida")})
+    public ResponseEntity<Cliente> createCliente(@RequestBody ClienteVO clienteVo) {
+        Cliente cliente = new Cliente();
+        cliente.setNombreCli(clienteVo.getNombreCli());
+        cliente.setApellidoCli(clienteVo.getApellidoCli());
+        cliente.setDireccionCli(clienteVo.getDireccionCli());
+        cliente.setTelefonoCli(clienteVo.getTelefonoCli());
+        cliente.setEmailCli(clienteVo.getEmailCli());
+        return new ResponseEntity<>(this.clienteService.create(cliente), HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{identificacion}")
+    //@ApiOperation(value = "Actualizar Cliente", notes = "Servicio para actulizar un nuevo cliente")
+    //@ApiResponses(value = {@ApiResponse(code = 201, message = "Cliente actualizado correctamente"),
+      //      @ApiResponse(code = 404, message = "Cliente no encotrado")})
+    public ResponseEntity<Cliente> updateCliente(@PathVariable("identificacion") String identificacion, ClienteVO clienteVo) {
+        Cliente cliente = this.clienteService.findByIdentificacion(identificacion);
+        if (cliente == null) {
+            return new ResponseEntity<Cliente>(HttpStatus.NOT_FOUND);
+        } else {
+            cliente.setNombreCli(clienteVo.getNombreCli());
+            cliente.setApellidoCli(clienteVo.getApellidoCli());
+            cliente.setDireccionCli(clienteVo.getDireccionCli());
+            cliente.setTelefonoCli(clienteVo.getTelefonoCli());
+            cliente.setEmailCli(clienteVo.getEmailCli());
+
+        }
+        return new ResponseEntity<>(this.clienteService.update(cliente), HttpStatus.OK);
+    }
+
+    /**
+     * metodo que borra el cliente
+     * @param identificacion
+     */
+
+    @DeleteMapping("/{identificacion}")
+    @Operation(summary = "Cliente Eliminado")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Servicio para eliminar un cliente",
+                    content = { @Content(mediaType = "/{identificacion}",
+                            schema = @Schema(implementation = Cliente.class)) }),
+            @ApiResponse(responseCode = "201", description = "Cliente eliminado correctamente",
+                    content = @Content),
+            @ApiResponse(responseCode = "404", description = " Cliente no encontrado",
+                    content = @Content) })
+    public void removeCliente(@PathVariable("identificacion") String identificacion) {
+        //realizamos la consulta del cliente
+        Cliente cliente = this.clienteService.findByIdentificacion(identificacion);
+        if (cliente != null) {
+            this.clienteService.delete(cliente);
+        }
+    }
+
+    @GetMapping
+
+    @Operation(method = "Listar Cliente", description = "Servicio para listar todos los clientes")
+    @ApiResponses(value = {@ApiResponse(responseCode = "201", description =  "Clientes Encontrados"),
+            @ApiResponse(responseCode= "404", description = "Clientes no encontrados")})
+    public ResponseEntity<List<Cliente>> findAll() {
+
+        return ResponseEntity.ok(this.clienteService.findAll());
+    }
 }
