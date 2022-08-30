545d0cb272be172c7f2031c7b9cd124fd14738f0
pablovass <faktory.fm@gmail.com>
2022-08-30 11:36:44 -0300

    Spring security ho right

--- src/main/java/com/pablovass/EReservationApplication.java
+++ src/main/java/com/pablovass/EReservationApplication.java
@@ -2,13 +2,15 @@ package com.pablovass;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @EnableWebMvc
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class EReservationApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(EReservationApplication.class,args);
 	}
 
--- src/main/java/com/pablovass/util/BasicConfiguration.java
+++ src/main/java/com/pablovass/util/BasicConfiguration.java
@@ -1,2 +1,26 @@
-package com.pablovass.util;public class BasicConfiguration {
+package com.pablovass.util;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/***
+ * clase para manejar la seguridad
+ * @author pablovass
+ */
+@Configuration
+@EnableWebSecurity
+public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+    protected  void Configure(HttpSecurity http)throws Exception{
+        http.authorizeRequests()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .and()
+                .httpBasic();
+    }
+
 }
